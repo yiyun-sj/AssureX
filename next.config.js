fa944137abd52c58ac3a5da76167ba00ae2faa55
yiyun-sj <67709970+yiyun-sj@users.noreply.github.com>
2023-07-29 15:53:30 -0400

    fix cors

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
-const withNextra = require('nextra')({
+import nextra from 'nextra'
+
+const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-module.exports = withNextra()
+module.exports = withNextra({
+  headers: async () => [
+    {
+      // matching all API routes
+      source: '/api/:path*',
+      headers: [
+        { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        { key: 'Access-Control-Allow-Origin', value: '*' },
+        {
+          key: 'Access-Control-Allow-Methods',
+          value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+        },
+        {
+          key: 'Access-Control-Allow-Headers',
+          value:
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+        },
+      ],
+    },
+  ],
+})
