04e9178b0d7a4feb585a4b7194cd4f9ef39ea8ea
yiyun-sj <67709970+yiyun-sj@users.noreply.github.com>
2023-08-07 20:05:01 -0400

    add account getter

---
+++ pages/api/account/[email].ts
@@ -0,0 +1,40 @@
+import { createConnection } from 'mysql2/promise'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+interface ReqData {
+  email: string
+}
+interface ResData {
+  address: number
+  email: string
+  name: string
+  balance: string
+}
+
+async function dbQuery({ email }: ReqData) {
+  const connection = await createConnection(process.env.DATABASE_URL)
+  const [data] = await connection.query(
+    'SELECT * FROM `accounts` WHERE `email` = ?',
+    [email]
+  )
+  connection.end()
+  return data
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResData[]>
+) {
+  const { method, query } = req
+  switch (method) {
+    case 'GET':
+      dbQuery({ email: query.email as string }).then((data) => {
+        res.status(200).json(data as ResData[])
+      })
+      break
+    default:
+      res.setHeader('Allow', ['GET'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+      break
+  }
+}
